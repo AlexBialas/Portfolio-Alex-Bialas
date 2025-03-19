d59edb6f1114920d78e7c97975c3fbc28a3134f9
dci-student <aleksandrabialas94@gmail.com>
2025-03-19 10:21:29 +0100

    added hero & navbar

---
+++ Portfolio-Alex-Bialas/src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  const listNavbar = [
+    { name: "Home", link: "#home" },
+    { name: "Home", link: "#home" },
+    { name: "Home", link: "#home" },
+    { name: "Home", link: "#home" },
+    { name: "Home", link: "#home" },
+  ];
+  return <div></div>;
+}
