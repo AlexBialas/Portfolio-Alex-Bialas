3b2f097389a4500b0562391fdb7b361c6e86eb2b
dci-student <aleksandrabialas94@gmail.com>
2025-03-19 13:23:28 +0100

    added aos

--- Portfolio-Alex-Bialas/src/App.jsx
+++ Portfolio-Alex-Bialas/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./components/hero/Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
     <main className="bg-[#0d182e]">
       <Hero />
--- Portfolio-Alex-Bialas/src/components/hero/Hero.jsx
+++ Portfolio-Alex-Bialas/src/components/hero/Hero.jsx
@@ -10,11 +10,17 @@ const Hero = () => {
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col">
       <div className="md:h-[720px] h-[960px] md:w-[640px] w-[880px] right-0 bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)]"></div>
       <Navbar />
-      <section className="text-white body-font z-10">
+      <section
+        data-aos="fade-up"
+        data-aos-delay="250"
+        className="text-white body-font z-10"
+      >
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center relative">
             <img
               src={hi}
+              data-aos="fade-up"
+              data-aos-delay="400"
               alt="Greeting"
               className="absolute top-[350px] left-44 md:top-[-30px] md:left-[450px] transform -translate-x-1/2 -translate-y-1/2 w-20 h-20"
             />
