fc3ea3957013ec8ed7ebbf6b9dc28df0ff75d949
dci-student <aleksandrabialas94@gmail.com>
2025-03-27 13:38:28 +0100

    commit

--- Portfolio-Alex-Bialas/src/components/contact/Contact.jsx
+++ Portfolio-Alex-Bialas/src/components/contact/Contact.jsx
@@ -4,14 +4,14 @@ export default function Contact({ darkMode }) {
   return (
     <section id="contact" data-aos="fade-up" data-aos-delay="400">
       <div
-        className={`py-8 lg:py-16 mx-auto max-w-screen-md ${
+        className={`p-8 lg:py-16 mx-auto max-w-screen-md ${
           darkMode
             ? "text-white hover:text-gray-400"
             : "text-gray-900 hover:text-[#a68f5f]"
         }`}
       >
         <h1
-          className={`text-4xl font-black text-center leading-snug mb-6 ${
+          className={`text-3xl md:text-4xl font-black text-center leading-snug mb-6 ${
             darkMode
               ? "text-white hover:text-gray-400"
               : "text-gray-900 hover:text-[#a68f5f]"
@@ -25,7 +25,10 @@ export default function Contact({ darkMode }) {
           className="space-y-8"
         >
           <div>
-            <label htmlFor="email" className="block mb-2 text-xl font-medium">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-lg md:text-xl font-medium"
+            >
               Email
             </label>
             <input
@@ -33,13 +36,16 @@ export default function Contact({ darkMode }) {
               id="email"
               className={`shadow-sm font-bold border ${
                 darkMode ? "border-white text-white" : "border-black text-black"
-              } text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 placeholder:text-gray-400`}
+              } text-sm md:text-base rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 placeholder:text-gray-400`}
               placeholder="name@exemple.com"
               required
             />
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-xl font-medium">
+            <label
+              htmlFor="message"
+              className="block mb-2 text-lg md:text-xl font-medium"
+            >
               Message
             </label>
             <textarea
@@ -47,7 +53,7 @@ export default function Contact({ darkMode }) {
               id="message"
               className={`shadow-sm font-bold border ${
                 darkMode ? "border-white text-white" : "border-black text-black"
-              } text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 placeholder:text-gray-400`}
+              } text-sm md:text-base rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 placeholder:text-gray-400`}
               placeholder="Leave a comment ..."
               required
             />
@@ -57,7 +63,7 @@ export default function Contact({ darkMode }) {
               darkMode
                 ? "bg-gradient-to-r from-gray-800 to-gray-600 shadow-lg text-white p-4"
                 : "bg-gradient-to-r from-[#c2b69b] via-[#b09a73] to-[#a68f5f] text-black"
-            } border-0 py-2 px-6 focus:outline-none hover:bg-gradient-to-l hover:shadow-lg shadow hover:shadow-xl rounded-lg text-lg transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer`}
+            } border-0 py-2 px-6 focus:outline-none hover:bg-gradient-to-l hover:shadow-lg shadow hover:shadow-xl rounded-lg text-lg md:text-xl transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer`}
           >
             Send Message
           </button>
--- Portfolio-Alex-Bialas/src/components/footer/Footer.jsx
+++ Portfolio-Alex-Bialas/src/components/footer/Footer.jsx
@@ -13,12 +13,12 @@ const Footer = ({ darkMode }) => {
     <footer
       className={`${
         darkMode
-          ? ""
+          ? "bg-gray-800 text-white"
           : "bg-gradient-to-r from-[#c2b69b] via-[#b09a73] to-[#a68f5f] text-black"
       } shadow`}
     >
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-ms sm:text-center">
+      <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col md:flex-row items-center justify-between">
+        <span className="text-sm sm:text-center mb-4 md:mb-0">
           Aleksandra Bialas{" "}
           <a href="#" className="hover:underline">
             2025
@@ -26,15 +26,16 @@ const Footer = ({ darkMode }) => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
           {listNavbar.map((item, index) => (
-            <a
-              key={index}
-              href={item.link}
-              className={`hover:underline transition-colors duration-300 ease-in-out me-4 md:me-6 px-2 py-1 rounded ${
-                darkMode ? "hover:text-[#ffffff]" : "hover:text-[#000000]"
-              }`}
-            >
-              {item.name}
-            </a>
+            <li key={index} className="me-4 md:me-6">
+              <a
+                href={item.link}
+                className={`hover:underline transition-colors duration-300 ease-in-out px-2 py-1 rounded ${
+                  darkMode ? "hover:text-[#ffffff]" : "hover:text-[#000000]"
+                }`}
+              >
+                {item.name}
+              </a>
+            </li>
           ))}
         </ul>
       </div>
