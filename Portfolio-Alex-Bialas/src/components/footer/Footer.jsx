3f8d8a7252243064a4e5e386f22832a76c92dab5
dci-student <aleksandrabialas94@gmail.com>
2025-03-29 08:09:37 +0100

    footer

--- Portfolio-Alex-Bialas/src/components/footer/Footer.jsx
+++ Portfolio-Alex-Bialas/src/components/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = ({ darkMode, translations }) => {
         <span className="text-sm sm:text-center mb-4 md:mb-0">
           {translations.footer.copyright}{" "}
           <a href="#" className="hover:underline">
-            2025
+            20255
           </a>
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
